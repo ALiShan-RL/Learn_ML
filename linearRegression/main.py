d35a12f2163ff003660cd8246fbcd5f175daf81e
JLU21luojing <luojing21@mails.jlu.edu.cn>
2021-10-28 14:51:36 +0800

    luo21-10-28 commit

--- linearRegression/main.py
+++ linearRegression/main.py
@@ -6,32 +6,57 @@ import numpy as np
 import matplotlib as mpl
 from sklearn.model_selection import train_test_split
 
+
 houseDataSets = datasets.load_boston() # 利用sklearn中的房价数据
 
-print(houseDataSets)
 
-data_X = houseDataSets.data[:, np.newaxis, 12]     # np.newaxis是将数据增加维数，选择第13列的数据
+# 使用了所有的参数，多元线性规划
+data_X = houseDataSets.data     # np.newaxis是将数据增加维数，选择第13列的数据
 data_Y = houseDataSets.target   # 输出数据
 train_X, test_X,  train_Y, test_Y = train_test_split(data_X, data_Y, test_size=0.3)   # 70%的数据用来训练模型  30%数据用来测试模型
 
-
 regr = linear_model.LinearRegression()
 regr.fit(train_X, train_Y)        # 线性回归训练
-
+a = regr.intercept_  # 截距
+b = regr.coef_  # 回归系数
+print("拟合参数:截距", a, ",回归系数：", b)
 predict_Y = regr.predict(test_X)  # 将测试集的数据带入模型中，求出预测数据
 
-# 画图
-mpl.rcParams['font.sans-serif'] = [u'SimHei']  # 用来正常显示中文标签
-mpl.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 
-plt.scatter(test_X, test_Y, color='black')    # 将测试集用点显示出来
-plt.plot(test_X, predict_Y, color='red', linewidth=1) # 画出预测的先
+plt.xlabel('range(len(predict_Y))', fontsize=12)
+plt.ylabel('house price', fontsize=12)
+plt.plot(range(len(predict_Y)), predict_Y, 'red', linewidth=2, label="predict data")
+plt.plot(range(len(test_Y)), test_Y, 'green', label="test data")
 
-plt.xlabel('lower status of the population', fontsize=12)
-plt.ylabel('lower status of the population', fontsize=12)
-plt.title(u'LinearRegression', fontsize=12)
 plt.show()
+
+
+# houseDataSets = datasets.load_boston() # 利用sklearn中的房价数据
 #
+# print(houseDataSets)
+# 只是用了第13列的特征，一元线性规划
+# data_X = houseDataSets.data[:, np.newaxis, 12]     # np.newaxis是将数据增加维数，选择第13列的数据
+# data_Y = houseDataSets.target   # 输出数据
+# train_X, test_X,  train_Y, test_Y = train_test_split(data_X, data_Y, test_size=0.3)   # 70%的数据用来训练模型  30%数据用来测试模型
+#
+#
+# regr = linear_model.LinearRegression()
+# regr.fit(train_X, train_Y)        # 线性回归训练
+#
+# predict_Y = regr.predict(test_X)  # 将测试集的数据带入模型中，求出预测数据
+#
+# # 画图
+# mpl.rcParams['font.sans-serif'] = [u'SimHei']  # 用来正常显示中文标签
+# mpl.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+#
+# plt.scatter(test_X, test_Y, color='black')    # 将测试集用点显示出来
+# plt.plot(test_X, predict_Y, color='red', linewidth=1) # 画出预测的先
+#
+# plt.xlabel('lower status of the population', fontsize=12)
+# plt.ylabel('house price', fontsize=12)
+# plt.title(u'LinearRegression', fontsize=12)
+# plt.show()
+# #
 
 
 
